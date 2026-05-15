3ea7dfc2f0d6f478a6f93511039721a40d6d5ff5
biela000 <86307737+biela000@users.noreply.github.com>
2026-05-15 10:53:30 +0200

    make GameHandler a singleton

--- MOBILE/app/src/main/java/com/jtjmpm/mobile/GameHandler.java
+++ MOBILE/app/src/main/java/com/jtjmpm/mobile/GameHandler.java
@@ -4,28 +4,61 @@ import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
-public class GameHandler extends WebSocketClient {
-    public GameHandler(URI uri) {
-        super(uri);
-    }
-    @Override
-    public void onOpen(ServerHandshake handshake) {
+public class GameHandler {
+    private static GameHandler instance;
+    private WebSocketClient client;
+    private GameHandler() {}
 
-    }
-
-    @Override
-    public void onMessage(String message) {
-
-    }
-
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-
+    public static GameHandler getInstance() {
+        if (instance == null) {
+            instance = new GameHandler();
+        }
+        return instance;
     }
 
-    @Override
-    public void onError(Exception ex) {
+    public void connect(String url) {
+        if (client != null && client.isOpen()) return;
 
+        try {
+            client = new WebSocketClient(new URI(url)) {
+                @Override
+                public void onOpen(ServerHandshake handshakedata) {
+
+                }
+
+                @Override
+                public void onMessage(String message) {
+
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+
+                }
+
+                @Override
+                public void onError(Exception ex) {
+
+                }
+            };
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void send(String message) {
+        if (client != null && client.isOpen()) {
+            client.send(message);
+        }
+    }
+
+    public void close() {
+        if (client != null) client.close();
+    }
+
+    public boolean isConnected() {
+        return client != null && client.isOpen();
     }
 }
