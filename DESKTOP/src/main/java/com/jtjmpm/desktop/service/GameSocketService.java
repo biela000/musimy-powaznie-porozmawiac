eac4156439f9b9a763370946e0c524a51aaaafd3
biela000 <86307737+biela000@users.noreply.github.com>
2026-05-22 15:14:53 +0200

    develop dekstop app a bit more

--- DESKTOP/src/main/java/com/jtjmpm/desktop/service/GameSocketService.java
+++ DESKTOP/src/main/java/com/jtjmpm/desktop/service/GameSocketService.java
@@ -61,6 +61,9 @@ public class GameSocketService extends WebSocketServer {
 
     private void handlePlayerMove(PlayerMoveMessage move) {
         System.out.println("Received " + move.move.size() + " rotation samples");
+        for (int i = 0; i < move.move.size(); i++) {
+            System.out.println(move.move.get(i).x + " " + move.move.get(i).y + " " + move.move.get(i).z);
+        }
     }
 
     public boolean hasClient() {
