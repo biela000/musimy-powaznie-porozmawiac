ee34fd6a49adf172ed278ade58149b655aa3c6c8
biela000 <86307737+biela000@users.noreply.github.com>
2026-05-22 02:50:05 +0200

    start and close socket server on correct moments

--- DESKTOP/src/main/java/com/jtjmpm/desktop/HelloApplication.java
+++ DESKTOP/src/main/java/com/jtjmpm/desktop/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.jtjmpm.desktop;
 
+import com.jtjmpm.desktop.service.GameSocketService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,12 +9,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private GameSocketService server;
     @Override
     public void start(Stage stage) throws IOException {
+        server = new GameSocketService(8080);
+        server.start();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        stage.setOnCloseRequest(e -> {
+            try {
+                server.stop();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 }
