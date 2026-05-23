37e46eb237199b93944ac86e18fee5521072f7d9
tomek-tomasz <226252648+tomek-tomasz@users.noreply.github.com>
2026-05-23 18:10:33 +0200

    added LobbyErrorMessage to handleJoinLobby

--- API/src/main/java/com/jtjmpm/api/game/GameHandler.java
+++ API/src/main/java/com/jtjmpm/api/game/GameHandler.java
@@ -113,7 +113,7 @@ public class GameHandler extends WebSocketServer {
                 String jsonResponse = gson.toJson(responseMessage);
                 conn.send(jsonResponse);
             }else{
-                LobbyErrorMessage errorMessage = new LobbyErrorMessage("Lobby named " + lobbyName + "already exists...");
+                LobbyErrorMessage errorMessage = new LobbyErrorMessage("Lobby named " + lobbyName + " already exists...");
                 String jsonResponse = gson.toJson(errorMessage);
                 conn.send(jsonResponse);
             }
@@ -129,12 +129,15 @@ public class GameHandler extends WebSocketServer {
         System.out.println("Joining a lobby: " + lobbyName + " " + sessionId + " is joining a lobby");
 
         try{
-            store.connectToLobby(lobbyName, sessionId);
-
-            GameStateUpdateMessage responseMessage = new GameStateUpdateMessage(store.getPlayersLobby(sessionId));
-            String jsonResponse = gson.toJson(responseMessage);
-            conn.send(jsonResponse);
-
+            if(store.connectToLobby(lobbyName, sessionId)){
+                GameStateUpdateMessage responseMessage = new GameStateUpdateMessage(store.getPlayersLobby(sessionId));
+                String jsonResponse = gson.toJson(responseMessage);
+                conn.send(jsonResponse);
+            }else{
+                LobbyErrorMessage errorMessage = new LobbyErrorMessage("Lobby named " + lobbyName + " is full or doesn't exist...");
+                String jsonResponse = gson.toJson(errorMessage);
+                conn.send(jsonResponse);
+            }
         } catch (Exception e) {
             System.err.println("Error while creating lobby from session: " + sessionId + ": " + e.getMessage());
             e.printStackTrace();
