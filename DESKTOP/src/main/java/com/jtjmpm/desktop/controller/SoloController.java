29499d07611fd64a6edc6c6412d91c56c0cc7faf
davxoo <dawix4321@gmail.com>
2026-05-24 03:07:06 +0200

    added new solo view classes

--- DESKTOP/src/main/java/com/jtjmpm/desktop/controller/SoloController.java
+++ DESKTOP/src/main/java/com/jtjmpm/desktop/controller/SoloController.java
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.List;
 
-public class GameController {
+public class SoloController {
 
     @FXML
     private Canvas gestureCanvas;
