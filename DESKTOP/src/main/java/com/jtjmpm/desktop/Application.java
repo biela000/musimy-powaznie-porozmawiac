e74b9ba3e6b1f7564836b13d0e1e59350f0dc157
biela000 <86307737+biela000@users.noreply.github.com>
2026-05-27 00:47:40 +0200

    remove unneeded files, rename wrongly-named files

--- DESKTOP/src/main/java/com/jtjmpm/desktop/Application.java
+++ DESKTOP/src/main/java/com/jtjmpm/desktop/Application.java
@@ -2,14 +2,13 @@ package com.jtjmpm.desktop;
 
 import com.jtjmpm.desktop.service.ApiSocketClient;
 import com.jtjmpm.desktop.service.GameSocketService;
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     private GameSocketService server;
 
     @Override
@@ -34,7 +33,7 @@ public class HelloApplication extends Application {
         });
 
         FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("lobby-view.fxml")
+                Application.class.getResource("lobby-view.fxml")
         );
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Game");
