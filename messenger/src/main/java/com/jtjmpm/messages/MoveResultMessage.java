330b7a1c402e9c606d1502e16d363385216f089a
biela000 <86307737+biela000@users.noreply.github.com>
2026-05-27 17:11:30 +0200

    fix point2d uses in messenger

--- messenger/src/main/java/com/jtjmpm/messages/MoveResultMessage.java
+++ messenger/src/main/java/com/jtjmpm/messages/MoveResultMessage.java
@@ -2,14 +2,15 @@ package com.jtjmpm.messages;
 
 import com.jtjmpm.MessageType;
 
+import java.awt.geom.Point2D;
 import java.util.List;
 
 public class MoveResultMessage extends WsMessage {
-    public List<Point2D> points;
+    public List<Point2D.Double> points;
     public double accuracy;
     public String playerID;
 
-    public MoveResultMessage(List<Point2D> points, double accuracy, String playerID) {
+    public MoveResultMessage(List<Point2D.Double> points, double accuracy, String playerID) {
         super(MessageType.MOVE_RESULT);
         this.points = points;
         this.accuracy = accuracy;
--- messenger/src/main/java/com/jtjmpm/messages/ShapeMessage.java
+++ messenger/src/main/java/com/jtjmpm/messages/ShapeMessage.java
@@ -2,13 +2,14 @@ package com.jtjmpm.messages;
 
 import com.jtjmpm.MessageType;
 
+import java.awt.geom.Point2D;
 import java.util.List;
 
 public class ShapeMessage extends WsMessage {
-    public List<Point2D> points;
+    public List<Point2D.Double> points;
     public double accuracy;
 
-    public ShapeMessage(List<Point2D> points, double accuracy) {
+    public ShapeMessage(List<Point2D.Double> points, double accuracy) {
         super(MessageType.SHAPE_DRAWN);
         this.points = points;
         this.accuracy = accuracy;
