8fb0e197e59064e60acebec9ae466663e4c8481f
biela000 <86307737+biela000@users.noreply.github.com>
2026-05-27 17:32:21 +0200

    change the way we contain move results

--- messenger/src/main/java/com/jtjmpm/messages/MoveResultMessage.java
+++ messenger/src/main/java/com/jtjmpm/messages/MoveResultMessage.java
@@ -1,19 +1,18 @@
 package com.jtjmpm.messages;
 
 import com.jtjmpm.MessageType;
+import com.jtjmpm.PlayerMoveResult;
 
 import java.awt.geom.Point2D;
 import java.util.List;
 
 public class MoveResultMessage extends WsMessage {
-    public List<Point2D.Double> points;
-    public double accuracy;
-    public String playerID;
+    public PlayerMoveResult result;
+    public String playerId;
 
-    public MoveResultMessage(List<Point2D.Double> points, double accuracy, String playerID) {
+    public MoveResultMessage(PlayerMoveResult result, String playerId) {
         super(MessageType.MOVE_RESULT);
-        this.points = points;
-        this.accuracy = accuracy;
-        this.playerID = playerID;
+        this.result = result;
+        this.playerId = playerId;
     }
 }
