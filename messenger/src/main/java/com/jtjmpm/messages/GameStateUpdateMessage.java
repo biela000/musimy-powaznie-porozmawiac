680913a4b9d665369a437f52e9566827aea65061
davxoo <dawix4321@gmail.com>
2026-06-01 01:13:06 +0200

    game state update message now has a list of event results

--- messenger/src/main/java/com/jtjmpm/messages/GameStateUpdateMessage.java
+++ messenger/src/main/java/com/jtjmpm/messages/GameStateUpdateMessage.java
@@ -3,10 +3,15 @@ package com.jtjmpm.messages;
 import com.jtjmpm.messages.GameStateDTO;
 import com.jtjmpm.MessageType;
 
+import java.util.List;
+
 public class GameStateUpdateMessage extends WsMessage{
     public GameStateDTO gameState;
-    public GameStateUpdateMessage(GameStateDTO gameState) {
+    public List<CombatEventMessage> events;
+
+    public GameStateUpdateMessage(GameStateDTO gameState, List<CombatEventMessage> events) {
         super(MessageType.GAME_STATE_UPDATE);
         this.gameState = gameState;
+        this.events = events;
     }
 }
