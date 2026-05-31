3866d2a936ae7dcac5b354f746c0c82daf9eec46
davxoo <dawix4321@gmail.com>
2026-05-31 12:41:53 +0200

    added mana bar

--- DESKTOP/src/main/java/com/jtjmpm/desktop/controller/PlayerPanelController.java
+++ DESKTOP/src/main/java/com/jtjmpm/desktop/controller/PlayerPanelController.java
@@ -12,9 +12,12 @@ import javafx.scene.paint.Color;
 public class PlayerPanelController {
 
     private static final double MAX_HP = 100.0;
+    private static final double MAX_MANA = 100.0;
 
     @FXML private ProgressBar hpBar;
     @FXML private Label hpLabel;
+    @FXML private ProgressBar manaBar;
+    @FXML private Label manaLabel;
     @FXML private Canvas canvas;
     @FXML private Label accuracyLabel;
 
@@ -30,10 +33,16 @@ public class PlayerPanelController {
 
     public void playerStateUpdate(PlayerDTO player) {
         updateHealthBar(player.hp());
+        updateManaBar(player.mana());
     }
 
     private void updateHealthBar(double myHp) {
         hpBar.setProgress(myHp / MAX_HP);
         hpLabel.setText("HP: " + (int)myHp + "/" + (int)MAX_HP);
+    }
+
+    private void updateManaBar(double myMana) {
+        hpBar.setProgress(myMana / MAX_MANA);
+        hpLabel.setText("HP: " + (int)myMana + "/" + (int)MAX_MANA);
     }
 }
