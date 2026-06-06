5d1b6a6743ff1592c78b2983009d528d971ab594
tomek-tomasz <226252648+tomek-tomasz@users.noreply.github.com>
2026-06-07 00:49:17 +0200

    added effects

---
+++ API/src/main/java/com/jtjmpm/api/model/ShieldEffect.java
@@ -0,0 +1,26 @@
+package com.jtjmpm.api.model;
+
+public class ShieldEffect implements StatusEffect {
+    private final double accuracy;
+    private boolean expired = false;
+
+    public ShieldEffect(double accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    @Override
+    public double modifySpellIncomingDamage(double rawDamage) {
+        expired = true;
+        return rawDamage * (1.0 - (0.8 * accuracy));
+    }
+
+    @Override
+    public boolean isExpired() {
+        return expired;
+    }
+
+    @Override
+    public String getName() {
+        return "SHIELD";
+    }
+}
---
+++ API/src/main/java/com/jtjmpm/api/model/VampirismEffect.java
@@ -0,0 +1,30 @@
+package com.jtjmpm.api.model;
+
+import com.jtjmpm.api.game.CombatEngine;
+import com.jtjmpm.messages.CombatEventMessage;
+
+import java.util.List;
+
+public class VampirismEffect extends AbstractTemporaryPeriodicEffect {
+    private final double accuracy;
+
+    public VampirismEffect(double durationInSeconds, double accuracy) {
+        super(1.0, durationInSeconds);
+        this.accuracy = accuracy;
+    }
+
+    @Override
+    protected void applyEffectLogic(GameState state, String targetId, CombatEngine engine, List<CombatEventMessage> outEvents) {
+    }
+
+    @Override
+    public void onAttackLanded(CombatEventMessage event, GameState state, String myId, CombatEngine engine) {
+        double healAmount = event.value * 0.5 * accuracy;
+        engine.applyHeal(state, myId, healAmount);
+    }
+
+    @Override
+    public String getName() {
+        return "VAMPIRISM";
+    }
+}
